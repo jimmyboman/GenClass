2bd072ed50d274f4cf07a7182298d1f3c41f2a8c
jimmyboman <61195361+jimmyboman@users.noreply.github.com>
2020-02-24 09:03:24 +0100

    return, stride
    Changed return to numpy array of size (n, h, w, c).
    Added stride parameter
    Currently does not handle non-integer divisible image segments

--- segtools.py
+++ segtools.py
@@ -1,32 +1,35 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 ##### Function: imgseg
 # Takes a numpy array containing an image (img), and a numpy array
 # containing a mask of anomalies in img (mask). Segments the image
-# into sections of (height,width) and returns one array containing 
-# the image segments, and one array of the same length that has a
-# value of 1 if there is an anomaly in the image segment at that 
-# index, and zero otherwise.
+# into sections of (height,width) and returns one numpy array 
+# containing the image segments, and one array of the same length 
+# that has a value of 1 if there is an anomaly in the image segment
+# at that index, and zero otherwise.
 
-def imgseg(img, mask, height, width):
-    
-    imgparts = []
-    contains_anomaly = []
-    
+def imgseg(img, mask, height, width, channels, stride):
+     
     im_h, im_w = img.shape[:2]
 
-    x = range(0,im_w,width//2)
-    y = range(0,im_h,height//2)
+    x = range(0,im_w,stride)
+    y = range(0,im_h,stride)
 
-    for row in y[:-1]:
-        for col in x[:-1]:
+    imgparts = np.zeros((len(x)*len(y), height, width, channels))
+    contains_anomaly = np.zeros(len(x)*len(y))
+    
+    counter = 0
+    for row in y[:-2]:
+        for col in x[:-2]:
             imgpart = img[row:row+height,col:col+width]
             maskpart = mask[row:row+height,col:col+width]
-            
-            imgparts.append(imgpart)
+            try:
+                imgparts[counter] = imgpart
+            except:
+                break
             if np.sum(maskpart) > 0:
-                contains_anomaly.append(1)
-            else:
-                contains_anomaly.append(0)
+                contains_anomaly[counter] = 1
+            counter += 1
     return imgparts, contains_anomaly
 
